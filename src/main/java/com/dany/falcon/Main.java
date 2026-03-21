ae9677d7a633488f79bbfe050a0e219f53bde16c
__Dany__ <tolozadaniel5@gmail.com>
2026-03-21 13:30:37 -0300

    remove TODO comment from main method

--- src/main/java/com/dany/falcon/Main.java
+++ src/main/java/com/dany/falcon/Main.java
@@ -22,7 +22,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner sc =new Scanner(System.in);
 
         int intents =0;
